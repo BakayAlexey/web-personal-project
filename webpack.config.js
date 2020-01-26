f5ccc55353ac73f5b80f6b79d2b1edede5e7061d
Bakay Alexey <bakaylexa@gmail.com>
2020-01-26 23:45:28 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+let conf = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, './build/'),
+        filename: 'index.js?[hash]',
+        publicPath: 'build/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+        ]
+    },
+    resolve: {
+        alias: {
+            '~': path.resolve(__dirname, 'src'),
+            '~c': path.resolve(__dirname, 'src/components'),
+        },
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: 'source-map',
+};
+
+module.exports = conf;
